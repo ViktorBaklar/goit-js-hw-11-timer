ff20673caf092b5b7a90cbf7f91e09ed3d95c575
ViktorBaklar <baklarv@gmail.com>
2021-03-09 19:26:31 +0200

    add js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './styles.css';
+// import './styles.css';
 
 const refs = {
   timer: document.querySelector('#timer-1'),
@@ -7,3 +7,45 @@ const refs = {
   mins: document.querySelector('[data-value="mins"]'),
   secs: document.querySelector('[data-value="secs"]'),
 };
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.clocks();
+  }
+
+  clocks() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      if (deltaTime < 0) {
+        refs.timer.innerHTML = 'You Are Out Of Time';
+      }
+      const time = this.getTimeComponents(deltaTime);
+      this.updateTimer(time);
+    }, 1000);
+  }
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  updateTimer({ days, hours, mins, secs }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
+  }
+}
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Mar 9, 2022'),
+});
