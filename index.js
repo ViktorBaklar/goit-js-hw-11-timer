5010815d4603946f583382b952bb517e602c33f8
ViktorBaklar <baklarv@gmail.com>
2021-03-09 17:33:53 +0200

    add html & basic css

---
+++ index.js
@@ -0,0 +1,9 @@
+import './styles.css';
+
+const refs = {
+  timer: document.querySelector('#timer-1'),
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
